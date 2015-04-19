641fffa478317279fb1518d3662ef96337ceed6e
nontokozo <nontokozondlovu96@gmail.com>
2015-04-19 23:46:15 +0200

    question4

--- Question4.cpp
+++ Question4.cpp
@@ -1,8 +1,8 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 //                         COMPUTER METHODS 3 PRACTICAL TEST 1 (MOCK)
-// Name:
-// Student No:
-// Date: 
+// Name:Nontokozo
+// Student No:211509006
+// Date: 19/04/2015
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
 // ***** QUESTION 4 ****
@@ -11,3 +11,80 @@
 // 4.2 From class "Animal", derive a concrete class "Bird" and "Dog".  Print appropriate message for
 //     each method.
 // 4.3 Write a driver program which creates an object of each type and demonstrates POLYMORPHISM. 
+
+# include <iostream>
+using namespace std;
+
+class Animal
+{
+public:
+	virtual void name()
+	{
+
+	};
+	virtual void does()
+	{};
+
+protected:
+	string naam;
+	string doe;
+
+};
+
+class Bird : public Animal
+{
+public:
+	void name()
+	{
+		
+		cout << "Name:" << "Bird" << endl;
+	}
+
+	void does()
+	{
+		cout << "Fly" << endl;
+	}
+};
+
+
+class Dog : public Animal
+{
+public:
+	void name()
+	{
+		cout << "Name:" << "Dog" << endl;
+	}
+
+	void does()
+	{
+		cout << "buck" << endl;
+	}
+};
+
+int main()
+{
+	Animal *anmlprt[2];
+	Bird brdptr;
+	Dog dogptr;
+
+	anmlprt[0] = &brdptr;
+	anmlprt[1] = &dogptr;
+
+	for (int i = 0; i < 2; i++)
+	{
+		anmlprt[i]->name();
+		cout << " ";
+
+		anmlprt[i]->does();
+		cout << endl;
+
+		
+	}
+
+	system("pause");
+	return 0;
+
+
+
+
+}
